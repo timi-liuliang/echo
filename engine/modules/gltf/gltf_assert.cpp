cb8c74c1b5df69e18d15a75ce8da93ef2fe305e0
LiuLiang <qq79402005@gmail.com>
2018-05-07 20:38:17 +0800

    support gltf #69

--- engine/modules/gltf/gltf_assert.cpp
+++ engine/modules/gltf/gltf_assert.cpp
@@ -26,6 +26,12 @@ namespace Echo
 				return false;
 			}
 
+			if (!loadMeshes(j))
+			{
+				EchoLogError("gltf parse meshes failed when load resource [%s].", path.c_str());
+				return false;
+			}
+
 			return true;
 		}
 
@@ -96,4 +102,81 @@ namespace Echo
 
 		return true;
 	}
+
+	bool GltfAsset::loadMeshes(nlohmann::json& json)
+	{
+		if (json.find("meshes") == json.end())
+			return false;
+
+		nlohmann::json& meshes = json["meshes"];
+		if (!meshes.is_array())
+			return false;
+
+		m_meshes.resize(meshes.size());
+		for (ui32 i = 0; i < meshes.size(); i++)
+		{
+			if (meshes[i].find("name") != meshes[i].end())
+			{
+				if (!meshes[i]["name"].is_string())
+					return false;
+
+				m_meshes[i].m_name = meshes[i]["name"].get<std::string>();
+			}
+
+			if (meshes[i].find("primitives") == meshes[i].end())
+				return false;
+
+			nlohmann::json& primitives = meshes[i]["primitives"];
+			if (!primitives.is_array())
+				return false;
+
+			m_meshes[i].m_primitives.resize(primitives.size());
+			for (ui32 j = 0; j < primitives.size(); j++)
+			{
+				// indices
+				if (primitives[j].find("indices") != primitives[j].end())
+				{
+					if (!primitives[j]["indices"].is_number())
+						return false;
+
+					m_meshes[i].m_primitives[j].m_indices = primitives[j]["indices"].get<ui32>();
+				}
+
+				// material
+				if (primitives[j].find("material") != primitives[j].end())
+				{
+					if (!primitives[j]["material"].is_number())
+						return false;
+
+					m_meshes[i].m_primitives[j].m_material = primitives[j]["material"].get<ui32>();
+				}
+
+				// mode
+				if (primitives[j].find("mode") != primitives[j].end())
+				{
+					if (!primitives[j]["mode"].is_number())
+						return false;
+
+					m_meshes[i].m_primitives[j].m_mode = GltfPrimitiveMode(primitives[j]["mode"].get<ui8>());
+				}
+
+				if (primitives[j].find("attributes") == primitives[j].end())
+					return false;
+
+				nlohmann::json& attributes = primitives[j]["attributes"];
+				if (!attributes.is_object())
+					return false;
+
+				for (nlohmann::json::iterator it = attributes.begin(); it != attributes.end(); it++)
+				{
+					m_meshes[i].m_primitives[j].m_attributes[it.key()] = it.value();
+				}
+			}
+		}
+
+		return true;
+	}
+
+
+
 }
--- engine/modules/gltf/gltf_assert.h
+++ engine/modules/gltf/gltf_assert.h
@@ -5,12 +5,14 @@
 
 namespace Echo
 {
+	using GltfAttributes = std::unordered_map<String, ui32>;
+
 	struct GltfMetaInfo
 	{
-		String		m_copyright;
-		String		m_generator;
-		String		m_version;
-		String		m_minversion;
+		String	m_copyright;
+		String	m_generator;
+		String	m_version;
+		String	m_minversion;
 	};
 
 	struct GltfSceneInfo
@@ -19,14 +21,42 @@ namespace Echo
 		vector<ui32>::type	m_nodes;
 	};
 
+	enum GltfPrimitiveMode
+	{
+		Points = 0,
+		Lines,
+		LineLoop,
+		LineStrip,
+		Triangles,
+		TriangleStrip,
+		TriangleFan
+	};
+
+	struct GltfPrimitive
+	{
+		ui32				m_indices = -1;		// index to accessor containing the indices
+		ui32				m_material = -1;	// index to the material
+		GltfPrimitiveMode	m_mode = GltfPrimitiveMode::Triangles; // each attribute is mapped with his name and accessor index to the data
+		GltfAttributes		m_attributes;
+	};
+
+	struct GltfMeshInfo
+	{
+		String						m_name;
+		vector<float>::type			m_weights;
+		vector<GltfPrimitive>::type	m_primitives;
+	};
+
 	struct GltfAsset
 	{
 		GltfMetaInfo				m_metaInfo;
 		vector<GltfSceneInfo>::type	m_scenes;
+		vector<GltfMeshInfo>::type	m_meshes;
 
 		// load
 		bool load(const String& path);
 		bool loadAsset(nlohmann::json& json);
 		bool loadScenes(nlohmann::json& json);
+		bool loadMeshes(nlohmann::json& json);
 	};
 }
