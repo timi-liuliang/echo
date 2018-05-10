462f651787309d801c83c23ffb8bb10d5ba9b40e
LiuLiang <qq79402005@gmail.com>
2018-05-10 12:19:06 +0800

    add google test library #74

---
+++ tests/unittest/camera/camera.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <engine/core/camera/Camera.h>
+
+TEST(OrthoCamera, depthIsLinear)
+{
+	Echo::Vector3 vCam2DPos(0, 0, 0);
+	Echo::Vector3 vCam2DDir = -Echo::Vector3::UNIT_Z;
+
+	Echo::Camera orthoCamera(Echo::Camera::PM_ORTHO);
+	orthoCamera.setPosition(vCam2DPos);
+	orthoCamera.setDirection(vCam2DDir);
+	orthoCamera.setNearClip(-256.f);
+	orthoCamera.setFarClip(256.0f);
+	orthoCamera.update();
+
+	for (int i = 0; i < 1000; i++)
+	{
+		float lenght = i / 1000.f * 512.f;
+		Echo::Vector3 pos = orthoCamera.getPosition() + orthoCamera.getDirection() * lenght;
+
+		orthoCamera.getViewProjMatrix();
+		Echo::Vector4 outPos;
+		Echo::Matrix4::TransformVec4(outPos, Echo::Vector4(pos.x, pos.y, pos.z, 1.f), orthoCamera.getViewProjMatrix());
+
+		outPos = outPos / outPos.w;
+
+		float u = outPos.x;
+		float v = outPos.y;
+		float depth = outPos.z;
+
+		float normailzedLength = i / 1000.f;
+
+		// find the relation between "normalizedLength" with "depth"
+		int a = 10;
+	}
+}
--- tests/unittest/main.cpp
+++ tests/unittest/main.cpp
@@ -1,16 +1,4 @@
 #include <gtest/gtest.h>
-#include <cmath>
-
-double cubic(double d)
-{
-	return pow(d, 3);
-}
-
-
-TEST(testMath, myCubeTest)
-{
-	EXPECT_EQ(1000, cubic(10));
-}
 
 // main function
 int main(int argc, char* argv[])
