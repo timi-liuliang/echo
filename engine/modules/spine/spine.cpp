2591eb110e4129f29c158e92876f4adda7034a55
LiuLiang <qq79402005@gmail.com>
2018-05-22 14:39:44 +0800

    * support spine #97

--- engine/modules/spine/spine.cpp
+++ engine/modules/spine/spine.cpp
@@ -5,9 +5,10 @@
 #include "render/Material.h"
 #include "engine/core/script/lua/luaex.h"
 #include "engine/core/main/Root.h"
+#include <spine/spine.h>
 
 // 默认材质
-static const char* g_live2dDefaultMaterial = R"(
+static const char* g_spinDefaultMaterial = R"(
 <?xml version = "1.0" encoding = "utf-8"?>
 <material>
 <vs>#version 100
@@ -68,18 +69,7 @@ void main(void)
 namespace Echo
 {
 	Spine::Spine()
-		: m_mocRes("", ".moc3")
-		, m_textureRes("", ".png")
-		, m_curMotionRes("", ".json")
-		//, m_curMotion(nullptr)
-		, m_mocMemory(nullptr)
-		//, m_moc(nullptr)
-		//, m_model(nullptr)
-		, m_tableSize(0)
-		, m_tableMemory(nullptr)
-		//, m_table(nullptr)
-		, m_modelSize(0)
-		, m_modelMemory(nullptr)
+		: m_spinRes("", ".json")
 		, m_mesh(nullptr)
 		, m_materialInst(nullptr)
 		, m_renderable(nullptr)
@@ -93,247 +83,18 @@ namespace Echo
 
 	void Spine::bindMethods()
 	{
-		CLASS_BIND_METHOD(Spine, getMoc, DEF_METHOD("getMoc"));
-		CLASS_BIND_METHOD(Spine, setMoc, DEF_METHOD("setMoc"));
-		CLASS_BIND_METHOD(Spine, getTextureRes, DEF_METHOD("getTextureRes"));
-		CLASS_BIND_METHOD(Spine, setTextureRes, DEF_METHOD("setTextureRes"));
-		CLASS_BIND_METHOD(Spine, getMotionRes, DEF_METHOD("getMotionRes"));
-		CLASS_BIND_METHOD(Spine, setMotionRes, DEF_METHOD("setMotionRes"));
+		CLASS_BIND_METHOD(Spine, getSpin, DEF_METHOD("getSpin"));
+		CLASS_BIND_METHOD(Spine, setSpin, DEF_METHOD("setSpin"));
 
-		CLASS_REGISTER_PROPERTY(Spine, "Moc", Variant::Type::ResourcePath, "getMoc", "setMoc");
-		CLASS_REGISTER_PROPERTY(Spine, "Texture", Variant::Type::ResourcePath, "getTextureRes", "setTextureRes");
-		CLASS_REGISTER_PROPERTY(Spine, "Motion", Variant::Type::ResourcePath, "getMotionRes", "setMotionRes");
-	}
-
-	// parse paramters
-	void Spine::parseParams()
-	{
-		//int paramerCount = csmGetParameterCount(m_model);
-		//if (paramerCount > 0)
-		//{
-		//	m_params.resize(paramerCount);
-		//	const char** ids = csmGetParameterIds(m_model);
-		//	const float* curValues = csmGetParameterValues(m_model);
-		//	const float* defaultValues = csmGetParameterDefaultValues(m_model);
-		//	const float* minValues = csmGetParameterMinimumValues(m_model);
-		//	const float* maxValues = csmGetParameterMaximumValues(m_model);
-		//	for (int i = 0; i < paramerCount; i++)
-		//	{
-		//		Paramter& param = m_params[i];
-		//		param.m_name = ids[i];
-		//		param.m_value = curValues[i];
-		//		param.m_defaultValue = defaultValues[i];
-		//		param.m_minValue = minValues[i];
-		//		param.m_maxValue = maxValues[i];
-		//	}
-		//}
-	}
-
-	// parse parts
-	void Spine::parseParts()
-	{
-		//int partCount = csmGetPartCount(m_model);
-		//if (partCount > 0)
-		//{
-		//	m_parts.resize(partCount);
-		//	const char** ids = csmGetPartIds(m_model);
-		//	const float* opacities = csmGetPartOpacities(m_model);
-		//	for (int i = 0; i < partCount; i++)
-		//	{
-		//		Part& part = m_parts[i];
-		//		part.m_name = ids[i];
-		//		part.m_opacities = opacities[i];
-		//	}
-		//}
-	}
-
-	// parse drawables
-	void Spine::parseDrawables()
-	{
-		//m_localAABB.reset();
-
-		//int drawableCount = csmGetDrawableCount(m_model);
-		//if (drawableCount > 0)
-		//{
-		//	m_drawables.resize(drawableCount);
-		//	const char** ids = csmGetDrawableIds(m_model);
-		//	const csmFlags* constantFlags = csmGetDrawableConstantFlags(m_model);
-		//	const csmFlags* dynamicFlags = csmGetDrawableDynamicFlags(m_model);
-		//	const int* textureIndices = csmGetDrawableTextureIndices(m_model);
-		//	const int* drawOrders = csmGetDrawableDrawOrders(m_model);
-		//	const int* renderOrders = csmGetDrawableRenderOrders(m_model);
-		//	const float* opacities = csmGetDrawableOpacities(m_model);
-		//	const int*  maskCounts = csmGetDrawableMaskCounts(m_model);
-		//	const int** masks = csmGetDrawableMasks(m_model);
-		//	const int* vertexCounts = csmGetDrawableVertexCounts( m_model);
-		//	const csmVector2** positions = csmGetDrawableVertexPositions( m_model);
-		//	const csmVector2** uvs = csmGetDrawableVertexUvs( m_model);
-		//	const int* indexCounts = csmGetDrawableIndexCounts(m_model);
-		//	const unsigned short** indices = csmGetDrawableIndices( m_model);
-		//	for (int i = 0; i < drawableCount; i++)
-		//	{
-		//		// reference
-		//		Drawable& drawable = m_drawables[i];
-		//		drawable.reset();
-
-		//		drawable.m_name = ids[i];
-		//		drawable.m_constantFlag = constantFlags[i];
-		//		drawable.m_dynamicFlag = dynamicFlags[i];
-		//		drawable.m_textureIndex = textureIndices[i];
-		//		drawable.m_drawOrder = drawOrders[i];
-		//		drawable.m_renderOrder = renderOrders[i];
-		//		drawable.m_opacitie = opacities[i];
-		//		ui32 maskCount = maskCounts[i];
-		//		for (ui32 j = 0; j < maskCount; j++)
-		//		{
-		//			drawable.m_masks.push_back(masks[i][j]);
-		//		}
-		//		
-		//		// vertexs
-		//		ui32 vertexCount = vertexCounts[i];
-		//		drawable.m_box.reset();
-		//		for (ui32 j = 0; j < vertexCount; j++)
-		//		{
-		//			const csmVector2& pos = positions[i][j];
-		//			const csmVector2& uv = uvs[i][j];
-
-		//			VertexFormat vert;
-		//			vert.m_position = Vector3(pos.X * m_canvas.m_pixelsPerUnit, pos.Y * m_canvas.m_pixelsPerUnit, 0.f);
-		//			vert.m_uv = Vector2(uv.X, 1.f-uv.Y);
-
-		//			drawable.m_vertices.push_back( vert);
-		//			drawable.m_box.addPoint(vert.m_position);
-		//		}
-
-		//		// calc local aabb
-		//		m_localAABB.unionBox(drawable.m_box);
-
-		//		// indices
-		//		ui32 indeceCount = indexCounts[i];
-		//		for (ui32 j = 0; j < indeceCount; j++)
-		//		{
-		//			drawable.m_indices.push_back( indices[i][j]);
-		//		}
-		//	}
-
-		//	std::sort(m_drawables.begin(), m_drawables.end(), [](const Drawable& a, const Drawable& b) ->int {return a.m_renderOrder<b.m_renderOrder; });
-		//}
-	}
-
-	// parse canvas info
-	void Spine::parseCanvasInfo()
-	{
-		//csmVector2 sizeInPixels;
-		//csmVector2 originInPixels;
-		//float	   pixelsPerUnit;
-		//csmReadCanvasInfo(m_model, &sizeInPixels, &originInPixels, &pixelsPerUnit);
-
-		//m_canvas.m_width = sizeInPixels.X;
-		//m_canvas.m_height = sizeInPixels.Y;
-		//m_canvas.m_originInPixels = Vector2(originInPixels.X, originInPixels.Y);
-		//m_canvas.m_pixelsPerUnit = pixelsPerUnit;
+		CLASS_REGISTER_PROPERTY(Spine, "Spin", Variant::Type::ResourcePath, "getSpin", "setSpin");
 	}
 
 	// set moc
-	void Spine::setMoc(const ResourcePath& res)
+	void Spine::setSpin(const ResourcePath& res)
 	{
-		//if (m_mocRes.setPath(res.getPath()))
-		//{
-		//	clear();
-
-		//	m_mocMemory = EchoNew(MemoryReaderAlign(m_mocRes.getPath(), csmAlignofMoc));
-		//	if (m_mocMemory->getSize())
-		//	{
-		//		m_moc = csmReviveMocInPlace(m_mocMemory->getData<void*>(), m_mocMemory->getSize());
-		//		if (m_moc)
-		//		{
-		//			m_modelSize = csmGetSizeofModel(m_moc);
-		//			m_modelMemory = EchoMallocAlign(m_modelSize, csmAlignofModel);
-		//			m_model = csmInitializeModelInPlace(m_moc, m_modelMemory, m_modelSize);
-
-		//			m_tableSize = csmGetSizeofModelHashTable(m_model);
-		//			m_tableMemory = EchoMalloc(m_tableSize);
-		//			m_table = csmInitializeModelHashTableInPlace(m_model, m_tableMemory, m_tableSize);
-
-		//			parseCanvasInfo();
-		//			parseParams();
-		//			parseParts();
-		//			parseDrawables();
-
-		//			buildRenderable();
-		//		}
-		//	}
-		//}
-	}
-
-	// set texture res path
-	void Spine::setTextureRes(const ResourcePath& path)
-	{
-		if (m_textureRes.setPath(path.getPath()))
+		if (m_spinRes.setPath(res.getPath()))
 		{
-			clearRenderable();
-			buildRenderable();
-		}
-	}
-
-	// set anim res path
-	void Spine::setMotionRes(const ResourcePath& path)
-	{
-		//if (m_curMotionRes.setPath(path.getPath()))
-		//{
-		//	auto it = m_motions.find(path.getPath());
-		//	if (it == m_motions.end())
-		//	{
-		//		m_curMotion = EchoNew(Live2dCubismMotion(m_curMotionRes));
-		//		m_curMotion->play();
-		//		m_motions[path.getPath()] = m_curMotion;
-		//	}
-		//	else
-		//	{
-		//		m_curMotion = it->second;
-		//		m_curMotion->play();
-		//	}
-		//}
-	}
-
-	// set parameter value
-	void Spine::setParameter(const String& name, float value)
-	{
-		//float* curValues = csmGetParameterValues(m_model);
-		//for (size_t i = 0; i < m_params.size(); i++)
-		//{
-		//	if (m_params[i].m_name == name)
-		//	{
-		//		curValues[i] = value;
-		//		m_params[i].m_value = value;
-		//	}
-		//}
-	}
-
-	// build drawable
-	void Spine::buildRenderable()
-	{
-		if (!m_textureRes.getPath().empty() && !m_drawables.empty())
-		{
-			VertexArray	vertices;
-			IndiceArray	indices;
-			buildMeshDataByDrawables(vertices, indices);
-
-			MeshVertexFormat define;
-			define.m_isUseUV = true;
-
-			m_mesh = Mesh::create(true, true);
-			m_mesh->updateIndices(indices.size(), indices.data());
-			m_mesh->updateVertexs(define, vertices.size(), (const Byte*)vertices.data(), m_localAABB);
-
-			m_materialInst = MaterialInst::create();
-			m_materialInst->setOfficialMaterialContent(g_live2dDefaultMaterial);
-			m_materialInst->setRenderStage("Transparent");
-			m_materialInst->applyLoadedData();
-
-			m_materialInst->setTexture("u_BaseColorSampler", m_textureRes.getPath());
-
-			m_renderable = Renderable::create(m_mesh, m_materialInst, this);
+			int a = 10;
 		}
 	}
 
@@ -355,39 +116,6 @@ namespace Echo
 
 		//	m_renderable->submitToRenderQueue();
 		//}
-	}
-
-	// build mesh data by drawables data
-	void Spine::buildMeshDataByDrawables(VertexArray& oVertices, IndiceArray& oIndices)
-	{
-		for (Drawable& drawable : m_drawables)
-		{
-			ui32 vertOffset = oVertices.size();
-
-			// vertices
-			for (VertexFormat& vert : drawable.m_vertices)
-				oVertices.push_back(vert);
-
-			// indices
-			for (int idx : drawable.m_indices)
-				oIndices.push_back(idx + vertOffset);
-		}
-	}
-
-	// update vertex buffer
-	void Spine::updateMeshBuffer()
-	{
-		parseDrawables();
-
-		VertexArray	vertices;
-		IndiceArray	indices;
-		buildMeshDataByDrawables(vertices, indices);
-
-		MeshVertexFormat define;
-		define.m_isUseUV = true;
-
-		m_mesh->updateIndices(indices.size(), indices.data());
-		m_mesh->updateVertexs( define, vertices.size(), (const Byte*)vertices.data(), m_localAABB);
 	}
 
 	// 获取全局变量值
--- engine/modules/spine/spine.h
+++ engine/modules/spine/spine.h
@@ -11,58 +11,6 @@ namespace Echo
 	{
 		ECHO_CLASS(Spine, Node)
 
-		struct CanvasInfo
-		{
-			float		m_width;
-			float		m_height;
-			Vector2		m_originInPixels;
-			float	    m_pixelsPerUnit;
-		};
-
-		struct Paramter
-		{
-			String	m_name;
-			float	m_value;
-			float	m_defaultValue;
-			float	m_minValue;
-			float	m_maxValue;
-		};
-
-		struct Part
-		{
-			String  m_name;
-			float	m_opacities;
-		};
-
-		struct VertexFormat
-		{
-			Vector3		m_position;
-			Vector2		m_uv;
-		};
-		typedef vector<VertexFormat>::type	VertexArray;
-		typedef vector<Word>::type	IndiceArray;
-
-		struct Drawable
-		{
-			String				m_name;
-			//csmFlags			m_constantFlag;
-			//csmFlags			m_dynamicFlag;
-			ui32				m_textureIndex;
-			ui32				m_drawOrder;
-			ui32				m_renderOrder;
-			float				m_opacitie;
-			vector<ui32>::type	m_masks;
-			VertexArray			m_vertices;
-			Box					m_box;
-			vector<Word>::type	m_indices;
-
-			void reset()
-			{
-				m_vertices.clear();
-				m_indices.clear();
-			}
-		};
-
 	public:
 		Spine();
 		virtual ~Spine();
@@ -71,50 +19,14 @@ namespace Echo
 		static void bindMethods();
 
 		// set moc
-		void setMoc(const ResourcePath& res);
+		void setSpin(const ResourcePath& res);
 
 		// get moc
-		const ResourcePath& getMoc() { return m_mocRes; }
-
-		// set texture res path
-		void setTextureRes(const ResourcePath& path);
-
-		// get texture res
-		const ResourcePath& getTextureRes() { return m_textureRes; }
-
-		// set anim res path
-		void setMotionRes(const ResourcePath& path);
-
-		// get anim res path
-		const ResourcePath& getMotionRes() const { return m_curMotionRes; }
-
-		// set parameter value
-		void setParameter(const String& name, float value);
+		const ResourcePath& getSpin() { return m_spinRes; }
 
 	protected:
-		// build drawable
-		void buildRenderable();
-
 		// update
 		virtual void update();
-
-		// update vertex buffer
-		void updateMeshBuffer();
-
-		// parse paramters
-		void parseParams();
-
-		// parse canvas info
-		void parseCanvasInfo();
-
-		// parse parts
-		void parseParts();
-
-		// parse drawables
-		void parseDrawables();
-
-		// build mesh data by drawables data
-		void buildMeshDataByDrawables(VertexArray& oVertices, IndiceArray& oIndices);
 
 		// get global uniforms
 		virtual void* getGlobalUniformValue(const String& name);
@@ -124,28 +36,14 @@ namespace Echo
 		void clearRenderable();
 
 	private:
-		ResourcePath			m_mocRes;
-		ResourcePath			m_textureRes;
-		ResourcePath			m_curMotionRes;
-		//Live2dCubismMotion*		m_curMotion;
-		MemoryReaderAlign*		m_mocMemory;
-		//csmMoc*					m_moc;
-		ui32					m_modelSize;
-		void*					m_modelMemory;
-		//csmModel*				m_model;
-		ui32					m_tableSize;
-		void*					m_tableMemory;
-		//csmModelHashTable*		m_table;
-		CanvasInfo				m_canvas;
-		vector<Paramter>::type	m_params;
-		vector<Part>::type		m_parts;
-		vector<Drawable>::type	m_drawables;
-		//MotionMap				m_motions;
-
-		Mesh*					m_mesh;			// Geometry Data for render
-		MaterialInst*			m_materialInst;	// Material Instance
-		Renderable*				m_renderable;
-
-		Matrix4					m_matWVP;
+		ResourcePath				m_spinRes;
+		struct spAtlas*				m_spAtlas;
+		struct spSkeleton*			m_spSkeleton;
+		struct spAnimationState*	m_spAnimState;
+		struct spAttachmentLoader*	m_attachmentLoader;
+		Mesh*						m_mesh;				// Geometry Data for render
+		MaterialInst*				m_materialInst;		// Material Instance
+		Renderable*					m_renderable;
+		Matrix4						m_matWVP;
 	};
 }
