5deb197051f9e6cb97702f4187fff1f97c90d05d
LiuLiang <qq79402005@gmail.com>
2018-09-15 16:32:30 +0800

    2d camera support rotation by z axis #208

--- engine/core/camera/Camera.h
+++ engine/core/camera/Camera.h
@@ -22,34 +22,32 @@ namespace Echo
 		virtual ~Camera();
 
 		// set
+		void setProjectionMode(ProjMode mode);
 		void setPosition(const Vector3& pos);
 		void setDirection(const Vector3& dir);
 		void setUp(const Vector3& vUp);
 
 		const Vector3& getPosition() const { return m_position; }
 		const Vector3& getDirection() const { return m_dir; }
-		const Vector3& getRight() const { return m_right; }
-
-		// 获取上方向
 		const Vector3& getUp() const { return m_up; }
+		const Vector3& getRight() const { return m_right; }
+		ProjMode getProjectionMode() const;
+
 		const Matrix4& getViewMatrix() const { return m_matView; }
 		const Matrix4& getProjMatrix() const { return m_matProj; }
 		const Matrix4& getViewProjMatrix() const { return m_matVP; }
-
-		void	setProjectionMode(ProjMode mode);
-		ProjMode getProjectionMode() const;
 
 		void setFov(Real fov);
 		void setWidth(Real width);
 		void setHeight(Real height);
 		void setScale(Real scale);
-		void			setNearClip(Real nearClip);
-		void			setFarClip(Real farClip);
-		Real			getFov() const;
-		Real			getWidth() const;
-		Real			getHeight() const;
-		const Real&		getNearClip() const;
-		const Real&		getFarClip() const;
+		void setNearClip(Real nearClip);
+		void setFarClip(Real farClip);
+		Real getFov() const;
+		Real getWidth() const;
+		Real getHeight() const;
+		const Real&	getNearClip() const;
+		const Real&	getFarClip() const;
 
 		void getCameraRay(Ray& ray, const Vector2& screenPos);
 		void unProjectionMousePos( Vector3& from, Vector3& to, const Vector2& screenPos );
