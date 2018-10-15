51ec0cb545250b8ced391e953216fc1c92871e4d
LiuLiang <qq79402005@gmail.com>
2018-10-15 11:14:10 +0800

    Bug in gltf pbr material. #220

--- engine/core/render/interface/TextureCube.h
+++ engine/core/render/interface/TextureCube.h
@@ -21,27 +21,27 @@ namespace Echo
 		virtual TexType getType() const override { return TT_Cube; }
 
 	public:
-		// positive x
+		// surface positive x
 		void setPositiveX(const ResourcePath& path);
 		const ResourcePath& getPositiveX() { return m_surfaces[0]; }
 
-		// positive x
+		// surface negative x
 		void setNegativeX(const ResourcePath& path);
 		const ResourcePath& getNegativeX() { return m_surfaces[1]; }
 
-		// positive x
+		// surface positive y
 		void setPositiveY(const ResourcePath& path);
 		const ResourcePath& getPositiveY() { return m_surfaces[2]; }
 
-		// positive x
+		// surface negative y
 		void setNegativeY(const ResourcePath& path);
 		const ResourcePath& getNegativeY() { return m_surfaces[3]; }
 
-		// positive x
+		// surface positive z
 		void setPositiveZ(const ResourcePath& path);
 		const ResourcePath& getPositiveZ() { return m_surfaces[4]; }
 
-		// positive x
+		// surface negative z
 		void setNegativeZ(const ResourcePath& path);
 		const ResourcePath& getNegativeZ() { return m_surfaces[5]; }
 
