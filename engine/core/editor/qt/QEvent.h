f0699d9e9e00efd2291536e7bffcb77d4ab6b8bd
qq79402005@gmail.com <qq79402005@gmail.com>
2019-01-01 23:14:07 +0800

    Support Scale Timeline QGraphicsView #270

---
+++ engine/core/editor/qt/QEvent.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "QWidget.h"
+
+#ifdef ECHO_EDITOR_MODE
+
+namespace Echo
+{
+	struct qEventAll
+	{
+		// QGraphicsWheelEvent
+		struct qGraphicsSceneWheelEvent {
+
+		}	graphicsSceneWheelEvent;
+	};
+}
+
+#endif
