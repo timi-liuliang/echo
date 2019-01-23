097088767a816c55cacd06a0be41782fcd08318e
Liang Liu <qq79402005@gmail.com>
2019-01-23 11:17:24 +0800

    Create a physx ball in the physx scene #288

--- engine/modules/physx/physx_base.h
+++ engine/modules/physx/physx_base.h
@@ -1,5 +1,9 @@
 #pragma once
 
-#define NDEBUG
+#include <engine/core/base/echo_def.h>
+
+#if defined(ECHO_PLATFORM_IOS) || defined(ECHO_PLATFORM_MAC)
+	#define NDEBUG
+#endif
 
 #include <PxPhysicsAPI.h>
