27adfb8c7676176a6b240e155960d43594edf94b
Liang Liu <qq79402005@gmail.com>
2019-01-29 19:47:32 +0800

    Add houdini plugin #298

--- engine/core/main/plugin.cpp
+++ engine/core/main/plugin.cpp
@@ -1,4 +1,6 @@
 #include "Plugin.h"
+#include "engine/core/util/PathUtil.h"
+#include "engine/core/log/Log.h"
 
 #ifdef ECHO_PLATFORM_WINDOWS
 #define DYNLIB_HANDLE         HMODULE
@@ -9,6 +11,9 @@
 
 namespace Echo
 {
+	// plugins
+	static map<String, Plugin*>::type	g_plugins;
+
 	Plugin::Plugin()
 	{
 
@@ -37,7 +42,36 @@ namespace Echo
 	void Plugin::loadAllPlugins()
 	{
 #if defined(ECHO_PLATFORM_WINDOWS) && defined(ECHO_EDITOR_MODE)
+		typedef void(*LOAD_PLUGIN_FUN)();
 
+		// get plugin path
+		String pluginDir = PathUtil::GetCurrentDir() + "/plugins";
+		PathUtil::FormatPath(pluginDir, false);
+
+		// get all plugins
+		StringArray plugins;
+		PathUtil::EnumFilesInDir( plugins, pluginDir, false, false, true);
+
+		// iterate
+		for (String& pluginPath : plugins)
+		{
+			String name = PathUtil::GetPureFilename(pluginPath, false);
+			String symbolName = StringUtil::Format("load%sPlugin", name.c_str());
+
+			Plugin* plugin = EchoNew(Plugin);
+			plugin->load(pluginPath.c_str());
+			LOAD_PLUGIN_FUN pFunc = (LOAD_PLUGIN_FUN)plugin->getSymbol(symbolName.c_str());
+			if (pFunc)
+			{
+				(*pFunc)();
+			}
+			else
+			{
+				EchoLogError("Can't find symbol %s in plugin [%s]", symbolName.c_str(), pluginPath.c_str());
+			}
+
+			g_plugins[name] = plugin;
+		}
 #endif
 	}
 }
