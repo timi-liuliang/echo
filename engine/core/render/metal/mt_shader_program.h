18f18b1d801e6ce6547d002011b15fff204f51fd
liuliang <qq79402005@gmail.com>
2019-02-02 23:06:54 +0800

    Draw a Triangle with Metal #278

--- engine/core/render/metal/mt_shader_program.h
+++ engine/core/render/metal/mt_shader_program.h
@@ -4,10 +4,16 @@
 
 namespace Echo
 {
+    /**
+     * https://developer.apple.com/library/archive/documentation/Miscellaneous/Conceptual/MetalProgrammingGuide/Prog-Func/Prog-Func.html
+     */
 	class MTShaderProgram : public ShaderProgram
 	{
     public:
         // link
         virtual bool linkShaders() override {return true;}
+        
+    private:
+        id<MTLLibrary>   m_metalLibrary;
 	};
 }
