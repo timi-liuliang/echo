b2c50b44cafefd3f419f2f10a0db48a6474ea30a
liuliang <qq79402005@gmail.com>
2019-02-05 20:03:22 +0800

    Draw a Triangle with Metal #278

--- engine/core/render/metal/mt_shader_program.mm
+++ engine/core/render/metal/mt_shader_program.mm
@@ -1,35 +1,52 @@
 #include "mt_shader_program.h"
 #include "mt_renderer.h"
+#include "engine/core/log/Log.h"
 
 static const char* testMetalLibrary = R"(
 // vertex function
-vertex float4 vertexShader(device float4 *vertices [[buffer(0)]], uint vid [[vertex_id]]){
-    
+vertex float4 vertexShader(device float4 *vertices [[buffer(0)]], uint vid [[vertex_id]])
+{
     return vertices[vid];
-    
 }
 
 // fragment function
-fragment float4 fragmentShader(float4 in [[stage_in]]){
-    
+fragment float4 fragmentShader(float4 in [[stage_in]])
+{
     //set color fragment to red
     return float4(1.0,0.0,0.0,1.0);
-    
-})";
+}
+)";
 
 namespace Echo
 {
     // create shader library
+    // https://fuchsia.googlesource.com/third_party/glfw/+/70297aeb493541072545760c379dd170ba54acbb/examples/metal.m
     bool MTShaderProgram::createShaderProgram(const String& vsContent, const String& psContent)
     {
         id<MTLDevice> device = MTRenderer::instance()->getMetalDevice();
         if(device)
         {
             NSString* sourceCode = [NSString stringWithUTF8String:testMetalLibrary];
-            m_metalLibrary = [device newLibraryWithSource:sourceCode];
-            
-            m_metalVertexShader   = [m_metalLibrary newFunctionWithName:@"vertexShader"];
-            m_metalFragmentShader = [m_metalLibrary newFunctionWithName:@"fragmentShader"];
+            MTLCompileOptions* compileOptions = [MTLCompileOptions new];
+            compileOptions.languageVersion = MTLLanguageVersion1_1;
+            NSError* compileError = nullptr;
+            m_metalLibrary = [device newLibraryWithSource:sourceCode options:compileOptions error:&compileError];
+            if(!compileError)
+            {
+                m_metalVertexShader   = [m_metalLibrary newFunctionWithName:@"vertexShader"];
+                m_metalFragmentShader = [m_metalLibrary newFunctionWithName:@"fragmentShader"];
+                
+                return m_metalVertexShader && m_metalFragmentShader ? true : false;
+            }
+            else
+            {
+                NSString* nsError = [NSString stringWithFormat:@"%@", compileError];
+                EchoLogError("%s", [nsError UTF8String]);
+                
+                return false;
+            }
         }
+        
+        return false;
     }
 }
