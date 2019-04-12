e6af043f925855c7d98740c3508d1f3eff9a8e18
Liang Liu <qq79402005@gmail.com>
2019-04-12 21:57:39 +0800

    Remove Render init implement from engine library #336

---
+++ editor/echo/Editor/Logic/Config/RenderConfig.cpp
@@ -0,0 +1,30 @@
+#include <engine/core/log/Log.h>
+#include <engine/core/render/gles/GLES.h>
+
+namespace Echo
+{
+	// initialize Renderer
+	Renderer* initRender(size_t hwnd, int width, int height)
+	{
+		Renderer* renderer = nullptr;
+#ifdef ECHO_PLATFORM_MAC
+		LoadMTRenderer(renderer);
+#else
+		LoadGLESRenderer(renderer);
+#endif
+
+		Echo::Renderer::Config renderCfg;
+		renderCfg.enableThreadedRendering = false;
+		renderCfg.windowHandle = hwnd;
+
+		EchoLogDebug("Canvas Size : %d x %d", renderCfg.screenWidth, renderCfg.screenHeight);
+		if (renderer && renderer->initialize(renderCfg))
+		{
+			EchoLogInfo("Init %s Renderer success.", renderer->getName());
+			return renderer;
+		}
+
+		EchoLogError("Root::initRenderer failed...");
+		return renderer;
+	}
+}
--- engine/core/render/interface/Renderer.cpp
+++ engine/core/render/interface/Renderer.cpp
@@ -191,29 +191,4 @@ namespace Echo
 		destroyRenderables(renderables.data(), static_cast<int>(renderables.size()));
 		renderables.clear();
 	}
-    
-    // initialize Renderer
-    Renderer* initRender(size_t hwnd, int width, int height)
-    {
-        Renderer* renderer = nullptr;
-#ifdef ECHO_PLATFORM_MAC
-        LoadMTRenderer(renderer);
-#else
-        LoadGLESRenderer(renderer);
-#endif
-        
-        Echo::Renderer::Config renderCfg;
-        renderCfg.enableThreadedRendering = false;
-        renderCfg.windowHandle = hwnd;
-        
-        EchoLogDebug("Canvas Size : %d x %d", renderCfg.screenWidth, renderCfg.screenHeight);
-        if (renderer && renderer->initialize(renderCfg))
-        {
-            EchoLogInfo("Init %s Renderer success.", renderer->getName());
-            return renderer;
-        }
-        
-        EchoLogError("Root::initRenderer failed...");
-        return renderer;
-    }
 }
