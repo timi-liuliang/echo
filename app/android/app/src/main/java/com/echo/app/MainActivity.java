c753c9c93372dec64882d332532ea91023575ab8
Liang Liu <qq79402005@gmail.com>
2019-04-19 13:26:34 +0800

    Compile for android platform with android studio and cmake #339

--- app/android/app/src/main/java/com/echo/app/MainActivity.java
+++ app/android/app/src/main/java/com/echo/app/MainActivity.java
@@ -10,4 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
 }
