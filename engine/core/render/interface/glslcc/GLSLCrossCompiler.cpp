ec6e3e442c9a88eb71626892e2ed51eee976f4bc
liuliang <qq79402005@gmail.com>
2019-05-04 00:40:48 +0800

    convert glsl to spirv #351

--- engine/core/render/interface/glslcc/GLSLCrossCompiler.cpp
+++ engine/core/render/interface/glslcc/GLSLCrossCompiler.cpp
@@ -1,5 +1,8 @@
 #include "GLSLCrossCompiler.h"
 #include <thirdparty/glslang/glslang/Public/ShaderLang.h>
+#include <thirdparty/glslang/SPIRV/SpvTools.h>
+#include <thirdparty/glslang/SPIRV/GlslangToSpv.h>
+#include "engine/core/log/Log.h"
 
 namespace Echo
 {
@@ -20,11 +23,59 @@ namespace Echo
     
     void GLSLCrossCompiler::compileGlslToSpirv()
     {
+        // initialize process (wrong place)
+        glslang::InitializeProcess();
+        
         // create shader program
         glslang::TProgram* prog = EchoNew(glslang::TProgram);
         
+        // shader
+        const char* shaders[3] = { m_inputVS.c_str(), m_inputFS.c_str(), m_inputCS.c_str()};
+        EShLanguage types[3]   = { EShLangVertex, EShLangFragment, EShLangCompute};
+        for(int i=0; i<3; i++)
+        {
+            if(strlen(shaders[i]))
+            {
+                int shaderLen = strlen(shaders[i]);
+                int defaultVersion = 100; // 110 for desktop
+                
+                glslang::TShader* shader = new glslang::TShader(types[i]);
+                shader->setStringsWithLengths(&shaders[i], &shaderLen, 1);
+                shader->setInvertY(false);
+                shader->setEnvInput(glslang::EShSourceGlsl, types[i], glslang::EShClientVulkan, defaultVersion);
+                shader->setEnvClient(glslang::EShClientVulkan, glslang::EShTargetVulkan_1_1);
+                shader->setEnvTarget(glslang::EShTargetSpv, glslang::EShTargetSpv_1_0);
+                prog->addShader(shader);
+            }
+        }
+        
+        // link
+        EShMessages messages = EShMsgDefault;
+        if (prog->link(messages))
+        {
+            // Output and save SPIR-V for each shader
+            std::vector<uint32_t> spirv;
+            for (int i = 0; i < 3; i++)
+            {
+                glslang::TIntermediate* intermediate = prog->getIntermediate(types[i]);
+                if(intermediate)
+                {
+                    glslang::SpvOptions spvOptions;
+                    spvOptions.validate = true;
+                    spv::SpvBuildLogger spvBuildLogger;
+                    glslang::GlslangToSpv(*intermediate, spirv, &spvBuildLogger, &spvOptions);
+                    //spvBuildLogger.getAllMessages().empty()
+                }
+            }
+        }
+        
+        // delete shaders
+        
         // deallocate program
         prog->~TProgram();
+        
+        // finalize process (wrong place)
+        glslang::FinalizeProcess();
     }
     
     void GLSLCrossCompiler::compileSpirvToCross(ShaderLanguage language)
