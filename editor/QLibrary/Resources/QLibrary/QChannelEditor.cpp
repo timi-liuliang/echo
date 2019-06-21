d874f491dd9ffe0f21af8f6be8662ea1826b1c35
liuliang <qq79402005@gmail.com>
2019-06-22 02:00:27 +0800

    Show channel in green background #386

---
+++ editor/QLibrary/Resources/QLibrary/QChannelEditor.cpp
@@ -0,0 +1,71 @@
+#include "QChannelEditor.h"
+#include <QColorDialog>
+#include <engine/core/util/StringUtil.h>
+
+namespace QT_UI
+{
+	QChannelEditor::QChannelEditor( QWidget* parent)
+		: QPushButton( parent)
+	{ 
+		// 消息
+		connect( this, SIGNAL(clicked()), this, SLOT(OnSelectColor()));
+	}
+
+	void QChannelEditor::OnSelectColor()
+	{
+		QColor color = QColorDialog::getColor(QColor((int)(m_color.r*255.f), (int)(m_color.g*255.f), (int)(m_color.b*255.f), (int)(m_color.a*255.f)), this, QString("选择颜色"), QColorDialog::ShowAlphaChannel);
+		if( color.isValid())
+			SetColor( Echo::Color( color.red()/255.f, color.green()/255.f, color.blue()/255.f,color.alpha()/255.f));
+	}
+
+	void QChannelEditor::SetColor( string color)
+	{ 
+		m_color = Echo::StringUtil::ParseColor(color.c_str());
+	}
+
+	void QChannelEditor::SetColor( const Echo::Color& color)
+	{ 
+		m_color=color;
+	}
+
+	string QChannelEditor::GetColor()
+	{
+		string color = Echo::StringUtil::ToString(m_color);
+		return color;
+	}
+
+	bool QChannelEditor::ItemDelegatePaint( QPainter *painter, const QRect& rect, const string& val)
+	{
+		Echo::Color color = Echo::StringUtil::ParseColor(val.c_str());
+		QColor qColor((int)(color.r*255.f), (int)(color.g*255.f), (int)(color.b*255.f));
+
+		// color rect
+		QRect tRect =  QRect( rect.left()+3, rect.top()+3, rect.height()-6, rect.height()-6);
+		painter->setBrush( qColor);
+		painter->drawRect( tRect);
+		painter->setPen( QColor( 0, 0, 0));
+		painter->drawRect( tRect);
+
+		// text
+		Echo::String text = Echo::StringUtil::Format( "rgb(%d,%d,%d)", (int)(color.r*255.f), (int)(color.g*255.f), (int)(color.b*255.f),(int)(color.a*255.f));
+		QRect textRect( rect.left()+rect.height()+3, rect.top()+3, rect.width()-rect.height()-3, rect.height()-6);
+		QFont font = painter->font(); font.setBold(false);
+		painter->setFont(font);
+		painter->setPen(QColor( 232, 232, 232));
+		painter->drawText( textRect, Qt::AlignLeft, text.c_str());
+
+		return true;
+	}
+
+	QSize QChannelEditor::sizeHint() const
+	{
+		return size();
+	}
+
+	void QChannelEditor::paintEvent( QPaintEvent* event)
+	{
+		QPainter painter( this);
+
+		ItemDelegatePaint( &painter, rect(), GetColor());
+	}
+}
---
+++ editor/QLibrary/Resources/QLibrary/QChannelEditor.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <QPainter>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QWidget>
+#include <engine/core/math/color.h>
+
+using namespace std;
+
+namespace QT_UI
+{
+	class QChannelEditor : public QPushButton
+	{
+		Q_OBJECT
+	public:
+		QChannelEditor( QWidget* parent = 0);
+
+		// 设置颜色
+		void SetColor( string color);
+
+		// 设置颜色
+		void SetColor( const Echo::Color& color);
+
+		// 获取颜色
+		string GetColor();
+
+		// 获取颜色
+		const Echo::Color& GetColor() const { return m_color; }
+
+		// MVC渲染
+		static bool ItemDelegatePaint( QPainter *painter, const QRect& rect, const string& val);
+
+	protected:
+		// 重定义渲染
+		void paintEvent( QPaintEvent* event); 
+
+		// 大小
+		QSize sizeHint() const;
+
+	public slots:
+		// 选择颜色
+		void OnSelectColor();
+
+	private:
+		Echo::Color		m_color;
+	};
+}
