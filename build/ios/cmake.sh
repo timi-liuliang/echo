e7e6dcacc261cb913311dbc5893aec1f73326320
liuliang <qq79402005@gmail.com>
2019-10-16 01:04:56 +0800

    build for ios platform #442

--- build/ios/cmake.sh
+++ build/ios/cmake.sh
@@ -3,8 +3,13 @@
 
 echo "cmake - generate xcode project"
 
+id -un
+
+xcodebuild -version
+
 # modify environment
-export PATH="$PATH:/Applications/CMake.app/Contents/bin:/usr/local/bin"
+# :
+export PATH="$PATH:/Applications/CMake.app/Contents/bin:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin"
 echo $PATH
 
 # show cmake version
@@ -17,4 +22,4 @@ cd ../../solution
 echo "build dir :" $(pwd)
 
 # cmake - generate xcode project
-cmake -DCMAKE_BUILD_TYPE=RELEASE -DECHO_EDITOR_MODE=FALSE -DECHO_BUILD_PLATFORM_IOS=TRUE -DECHO_BUILD_PLATFORM_MAC=FALSE -G"Xcode" ../
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=RELEASE -DECHO_EDITOR_MODE=FALSE -DECHO_BUILD_PLATFORM_IOS=TRUE -DECHO_BUILD_PLATFORM_MAC=FALSE -G"Xcode" ../
