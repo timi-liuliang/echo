8f2e80ca8920d068cf85193dad826c909cd106c7
liuliang <qq79402005@gmail.com>
2019-10-20 12:13:40 +0800

    compile ios app failed bug. #450
    Update luaconf.h.

--- thirdparty/lua/luaconf.h
+++ thirdparty/lua/luaconf.h
@@ -781,12 +781,14 @@
 ** Local configuration. You can use this space to add your redefinitions
 ** without modifying the main part of the file.
 */
-//#if defined(__APPLE__)
-//    #include "TargetConditionals.h"
-//    #define system(s) ((s)==NULL ? 0 : -1)
-//#elif defined(__ANDROID__)
-//    #define system(s) ((s)==NULL ? 0 : -1)
-//#endif
+#if defined(__APPLE__)
+    #include "TargetConditionals.h"
+    #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
+        #define system(s) ((s)==NULL ? 0 : -1)
+    #endif
+#elif defined(__ANDROID__)
+    #define system(s) ((s)==NULL ? 0 : -1)
+#endif
 
 
 #endif
