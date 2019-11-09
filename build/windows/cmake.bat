458690c3aaa704f914294535a2b4995e96c536f9
captain <qq79402005@gmail.com>
2019-11-09 23:11:29 +0800

    build app for windows platform, copy src and res #444

--- build/windows/cmake.bat
+++ build/windows/cmake.bat
@@ -11,11 +11,14 @@ if not exist build mkdir build
 cd build
 
 :: generate visual studio solution
-call cmake.exe -DCMAKE_BUILD_TYPE=RELEASE -DECHO_EDITOR_MODE=FALSE -G"Visual Studio 15 2017 Win64" ../
+call cmake.exe -DECHO_EDITOR_MODE=FALSE -G"Visual Studio 15 2017 Win64" ../
 
 :: mklink data
 cd /D "%~dp0"
 if not exist .\bin\app\win64\Debug\data mklink /D .\bin\app\win64\Debug\data ..\Release\data
+
+:: mklink sln
+if not exist .\echo.sln mklink .\echo.sln build\echo.sln
 
 :: dely close
 timeout /t 10
