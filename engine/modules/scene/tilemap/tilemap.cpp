39fcc5a5231ba8adc632220966f2f52bb2ab9ab3
Captain <qq79402005@gmail.com>
2020-02-18 23:35:43 +0800

    Show TileMap Grid in editor #564

--- engine/modules/scene/tilemap/tilemap.cpp
+++ engine/modules/scene/tilemap/tilemap.cpp
@@ -21,8 +21,17 @@ namespace Echo
     {
         CLASS_BIND_METHOD(TileMap, getTileShape, DEF_METHOD("getTileShape"));
         CLASS_BIND_METHOD(TileMap, setTileShape, DEF_METHOD("setTileShape"));
+		CLASS_BIND_METHOD(TileMap, getWidth, DEF_METHOD("getWidth"));
+		CLASS_BIND_METHOD(TileMap, setWidth, DEF_METHOD("setWidth"));
+		CLASS_BIND_METHOD(TileMap, getHeight, DEF_METHOD("getHeight"));
+		CLASS_BIND_METHOD(TileMap, setHeight, DEF_METHOD("setHeight"));
+		CLASS_BIND_METHOD(TileMap, getTileSize, DEF_METHOD("getTileSize"));
+		CLASS_BIND_METHOD(TileMap, setTileSize, DEF_METHOD("setTileSize"));
 
         CLASS_REGISTER_PROPERTY(TileMap, "TileShape", Variant::Type::StringOption, "getTileShape", "setTileShape");
+        CLASS_REGISTER_PROPERTY(TileMap, "Width", Variant::Type::Int, "getWidth", "setWidth");
+        CLASS_REGISTER_PROPERTY(TileMap, "Height", Variant::Type::Int, "getHeight", "setHeight");
+        CLASS_REGISTER_PROPERTY(TileMap, "TileSize", Variant::Type::Vector2, "getTileSize", "setTileSize");
     }
 
     void TileMap::setTileShape(const StringOption& option)
--- engine/modules/scene/tilemap/tilemap.h
+++ engine/modules/scene/tilemap/tilemap.h
@@ -15,8 +15,23 @@ namespace Echo
         // tile shape
         const StringOption& getTileShape() const { return m_tileShape; }
         void setTileShape(const StringOption& option);
+
+		// width
+		i32 getWidth() const { return m_width; }
+        void setWidth(i32 width) { m_width = width; }
+
+		// height
+		i32 getHeight() const { return m_height; }
+        void setHeight(i32 height) { m_height = height; }
+
+		// grid size
+		const Vector2& getTileSize() const { return m_tileSize; }
+        void setTileSize(const Vector2& tileSize) { m_tileSize = tileSize; }
         
     private:
-        StringOption        m_tileShape = StringOption("Square", { "Square","Isometric","Hexagon" });
+        StringOption        m_tileShape = StringOption("Square", { "Square"/*,"Isometric","Hexagon"*/ });
+		i32                 m_width = 8;
+		i32                 m_height = 8;
+        Vector2             m_tileSize = Vector2(60.f, 60.f);
     };
 }
