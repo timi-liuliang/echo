0dd55130fa6148bbb70dc5c8143b5cc85c9d622a
Captain <qq79402005@gmail.com>
2020-07-22 01:34:53 +0800

    android platform replace icon #691

--- editor/echo/Editor/Modules/build/android_build_settings.cpp
+++ editor/echo/Editor/Modules/build/android_build_settings.cpp
@@ -3,6 +3,7 @@
 #include "engine/core/main/Engine.h"
 #include <engine/core/io/IO.h>
 #include <thirdparty/pugixml/pugixml.hpp>
+#include "FreeImageHelper.h"
 #include <engine/core/main/module.h>
 
 namespace Echo
@@ -29,9 +30,12 @@ namespace Echo
 		CLASS_BIND_METHOD(AndroidBuildSettings, setAppName, DEF_METHOD("setAppName"));
         CLASS_BIND_METHOD(AndroidBuildSettings, getIconRes,   DEF_METHOD("getIconRes"));
         CLASS_BIND_METHOD(AndroidBuildSettings, setIconRes,   DEF_METHOD("setIconRes"));
+		CLASS_BIND_METHOD(AndroidBuildSettings, getIconRoundRes, DEF_METHOD("getIconRoundRes"));
+		CLASS_BIND_METHOD(AndroidBuildSettings, setIconRoundRes, DEF_METHOD("setIconRoundRes"));
 
 		CLASS_REGISTER_PROPERTY(AndroidBuildSettings, "AppName", Variant::Type::String, "getAppName", "setAppName");
         CLASS_REGISTER_PROPERTY(AndroidBuildSettings, "Icon", Variant::Type::ResourcePath, "getIconRes", "setIconRes");
+		CLASS_REGISTER_PROPERTY(AndroidBuildSettings, "IconRound", Variant::Type::ResourcePath, "getIconRoundRes", "setIconRoundRes");
     }
 
 	ImagePtr AndroidBuildSettings::getPlatformThumbnail() const
@@ -43,6 +47,11 @@ namespace Echo
     {
         
     }
+
+	void AndroidBuildSettings::setIconRoundRes(const ResourcePath& path)
+	{
+
+	}
 
     void AndroidBuildSettings::setOutputDir(const String& outputDir)
     {
@@ -61,7 +70,7 @@ namespace Echo
 			copySrc();
 			copyRes();
 
-			//replaceIcon();
+			replaceIcon();
 			//replaceLaunchImage();
 
 			// overwrite config
@@ -145,6 +154,43 @@ namespace Echo
 		return PathUtil::IsDirExist(FinalResultPath) ? FinalResultPath : m_outputDir;
 	}
 
+	void AndroidBuildSettings::replaceIcon()
+	{
+		// icon
+		AppIconItem iconConfigs[] =
+		{
+			{72, "mipmap-hdpi"},
+			{48, "mipmap-mdpi"},
+			{96, "mipmap-xhdpi"},
+			{144, "mipmap-xxhdpi"},
+			{192, "mipmap-xxxhdpi"}
+		};
+
+		String iconFullPath = IO::instance()->convertResPathToFullPath(m_iconRes.getPath());
+		if (PathUtil::IsFileExist(iconFullPath))
+		{
+			for (const AppIconItem& item : iconConfigs)
+			{
+				String outputPath = m_outputDir + StringUtil::Format("app/android/app/src/main/res/%s/ic_launcher.png", item.m_folder.c_str());
+				PathUtil::DelPath(outputPath);
+
+				rescaleIcon(iconFullPath.c_str(), outputPath.c_str(), item.m_size, item.m_size);
+			}
+		}
+
+		String iconRoundFullPath = IO::instance()->convertResPathToFullPath(m_iconRoundRes.getPath());
+		if (PathUtil::IsFileExist(iconRoundFullPath))
+		{
+			for (const AppIconItem& item : iconConfigs)
+			{
+				String outputPath = m_outputDir + StringUtil::Format("app/android/app/src/main/res/%s/ic_launcher_round.png", item.m_folder.c_str());
+				PathUtil::DelPath(outputPath);
+
+				rescaleIcon(iconRoundFullPath.c_str(), outputPath.c_str(), item.m_size, item.m_size);
+			}
+		}
+	}
+
 	void AndroidBuildSettings::writeStringsXml()
 	{
 		pugi::xml_document doc;
@@ -196,5 +242,33 @@ namespace Echo
 			stream.write(moduleSrc.data(), moduleSrc.size());
 			stream.close();
 		}
+	}
+
+	bool AndroidBuildSettings::rescaleIcon(const char* iFilePath, const char* oFilePath, ui32 targetWidth, ui32 targetHeight)
+	{
+		std::string ext = PathUtil::GetFileExt(iFilePath).c_str();
+		FREE_IMAGE_FORMAT fileFMT = FreeImage_GetFIFFromFilename(iFilePath);
+		int fiFlags = FreeImageHelper::instance()->MappingFlagsByFormat(fileFMT);
+
+		// Load
+		if (fileFMT != FIF_UNKNOWN && FreeImage_FIFSupportsReading(fileFMT))
+		{
+			FIBITMAP* dip = FreeImage_Load(fileFMT, iFilePath, fiFlags);
+			if (dip)
+			{
+				FIBITMAP* dipScaled = FreeImage_Rescale(dip, targetWidth, targetHeight, FILTER_BSPLINE);
+
+				dipScaled = FreeImage_ConvertTo32Bits(dipScaled);
+
+				FreeImage_Save(fileFMT, dipScaled, oFilePath, TARGA_DEFAULT);
+
+				FreeImage_Unload(dipScaled);
+				FreeImage_Unload(dip);
+
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
--- editor/echo/Editor/Modules/build/android_build_settings.h
+++ editor/echo/Editor/Modules/build/android_build_settings.h
@@ -7,6 +7,13 @@ namespace Echo
     class AndroidBuildSettings : public BuildSettings
     {
         ECHO_SINGLETON_CLASS(AndroidBuildSettings, BuildSettings)
+
+    public:
+		struct AppIconItem
+		{
+			i32     m_size;
+			String  m_folder;
+		};
         
     public:
         AndroidBuildSettings();
@@ -38,6 +45,9 @@ namespace Echo
         void setIconRes(const ResourcePath& path);
         const ResourcePath& getIconRes() { return m_iconRes; }
 
+		void setIconRoundRes(const ResourcePath& path);
+		const ResourcePath& getIconRoundRes() { return m_iconRoundRes; }
+
     private:
         // output directory
         bool prepare();
@@ -46,18 +56,26 @@ namespace Echo
         void copySrc();
         void copyRes();
 
+		// replace
+		void replaceIcon();
+
     private:
         // write settings
         void writeStringsXml();
 
         // write config
         void writeModuleConfig();
+
+    private:
+		// utils function
+		bool rescaleIcon(const char* iFilePath, const char* oFilePath, ui32 targetWidth, ui32 targetHeight);
         
     private:
         String                  m_rootDir;
         String                  m_projectDir;
         String                  m_outputDir;
-        ResourcePath            m_iconRes;
         String                  m_appName;
+        ResourcePath            m_iconRes = ResourcePath("Res://icon.png", ".png");
+        ResourcePath            m_iconRoundRes = ResourcePath("Res://icon.png", ".png");
     };
 }
