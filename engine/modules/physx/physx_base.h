5be3eb692b6fc0fae71f45042f4b7a2cd24c4ea2
Captain <qq79402005@gmail.com>
2020-08-09 07:44:12 -0700

    compile engine library for linux platform #702

--- engine/modules/physx/physx_base.h
+++ engine/modules/physx/physx_base.h
@@ -2,7 +2,7 @@
 
 #include <engine/core/base/echo_def.h>
 
-#if defined(ECHO_PLATFORM_IOS) || defined(ECHO_PLATFORM_MAC) || defined(ECHO_PLATFORM_ANDROID)
+#if defined(ECHO_PLATFORM_IOS) || defined(ECHO_PLATFORM_MAC) || defined(ECHO_PLATFORM_ANDROID) || defined(ECHO_PLATFORM_LINUX)
 	#define NDEBUG
 #endif
 
