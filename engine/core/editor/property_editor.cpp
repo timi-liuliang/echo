2695cb7a95fee10c78a9aefe80bc75d7df6e85a5
Captain <qq79402005@gmail.com>
2020-12-16 22:31:50 +0800

    Custom Property Editor for edit ParamterList #880

---
+++ engine/core/editor/property_editor.cpp
@@ -0,0 +1,16 @@
+#include "property_editor.h"
+
+namespace Echo
+{
+#ifdef ECHO_EDITOR_MODE
+	PropertyEditor::PropertyEditor()
+	{
+
+	}
+
+	PropertyEditor::~PropertyEditor()
+	{
+
+	}
+#endif
+}
---
+++ engine/core/editor/property_editor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "engine/core/base/echo_def.h"
+
+#ifdef ECHO_EDITOR_MODE
+namespace Echo
+{
+	class PropertyEditor
+	{
+	public:
+		PropertyEditor();
+		virtual ~PropertyEditor();
+
+
+	protected:
+	};
+
+}
+#endif
