ce7431f4792354646d293a5e3c43f5100fbc49dc
Captain <qq79402005@gmail.com>
2020-12-19 00:02:33 +0800

    Custom Property Editor for edit ParamterList #880

--- engine/core/editor/property_editor.cpp
+++ engine/core/editor/property_editor.cpp
@@ -1,8 +1,13 @@
 #include "property_editor.h"
+#include "engine/core/memory/MemAllocDef.h"
+#include "engine/core/log/Log.h"
+#include "engine/core/main/Engine.h"
 
 namespace Echo
 {
 #ifdef ECHO_EDITOR_MODE
+	static map<String, PropertyEditorFactory*>::type	g_propertyEditorFactorys;
+
 	PropertyEditor::PropertyEditor()
 	{
 
@@ -11,6 +16,34 @@ namespace Echo
 	PropertyEditor::~PropertyEditor()
 	{
 
+	}
+
+	void PropertyEditor::registerEditor(const char* className, const char* propertyName, PropertyEditorFactory* factory)
+	{
+		String finalName = StringUtil::Format("%s.%s", className, propertyName);
+		if (g_propertyEditorFactorys.find(finalName) == g_propertyEditorFactorys.end())
+		{
+			g_propertyEditorFactorys[finalName] = factory;
+		}
+		else
+		{
+			EchoLogError("property editor factory for [%s] has existed", finalName);
+		}
+	}
+
+	PropertyEditor* PropertyEditor::createEditor(const char* className, const char* propertyName)
+	{
+		if (!Engine::instance()->getConfig().m_isGame)
+		{
+			String finalName = StringUtil::Format("%s.%s", className, propertyName);
+			auto it = g_propertyEditorFactorys.find(finalName);
+			if (it != g_propertyEditorFactorys.end())
+			{
+				return it->second->create();
+			}
+		}
+
+		return nullptr;
 	}
 #endif
 }
--- engine/core/editor/property_editor.h
+++ engine/core/editor/property_editor.h
@@ -5,15 +5,44 @@
 #ifdef ECHO_EDITOR_MODE
 namespace Echo
 {
+	struct PropertyEditorFactory;
 	class PropertyEditor
 	{
 	public:
 		PropertyEditor();
 		virtual ~PropertyEditor();
 
+		// register|create editor
+		static void registerEditor(const char* className, const char* propertyName, PropertyEditorFactory* factory);
+		static PropertyEditor* createEditor(const char* className, const char* propertyName);
+
 
 	protected:
 	};
 
+	struct PropertyEditorFactory
+	{
+		virtual PropertyEditor* create() = 0;
+	};
+
+	template<typename T>
+	struct PropertyEditorFactoryT : public PropertyEditorFactory
+	{
+		PropertyEditorFactoryT(const char* className, const char* propertyName)
+		{
+			PropertyEditor::registerEditor(className, propertyName, this);
+		}
+
+		virtual PropertyEditor* create() override
+		{
+			return EchoNew(T);
+		}
+	};
 }
+
+#define REGISTER_PROPERTY_EDITOR(OBJECT, PROPERTY, EDITOR) static Echo::PropertyEditorFactoryT<EDITOR> G_PROPERTY_EDIT_##EDITOR##_REGISTER(#OBJECT, #PROPERTY);
+#else
+#define REGISTER_PROPERTY_EDITOR(OBJECT, PROPERTY, EDITOR)
 #endif
+
+
