452c63801ce03a45c2a7ce4ffbb54cb6c1f4452b
Captain <qq79402005@gmail.com>
2021-01-10 01:13:15 +0800

    Implement radial blur #893

--- engine/core/render/base/editor/shader/node/blur/shader_node_gaussian_blur.cpp
+++ engine/core/render/base/editor/shader/node/blur/shader_node_gaussian_blur.cpp
@@ -2,27 +2,29 @@
 
 #ifdef ECHO_EDITOR_MODE
 
-static const char* gaussianBlur =R"(vec3 GaussianBlur(sampler2D tex,vec2 uv,float dirs)
+static const char* gaussianBlur =R"(vec3 GaussianBlur(sampler2D tex,vec2 uv, float radius, float dirs, float quality, float strength)
 {
 	// https://www.shadertoy.com/view/Xltfzj
 	float pi = 6.28318530718;
 	float directions = dirs;	// blur directions (default 16.0 - more is better but slower)
-	float quality = 3.0;		// blur quality (default 4.0 - more is better but slower)
-	float radius = 8.0 / 512.0;	// blur radius
+	float step_radius = radius / 512.0;	// blur radius
 
-	vec4 color = texture(tex, uv);
-
+	vec4 origin = texture(tex, uv);
+	vec4 color = origin;
+	float count = 1.0;
+	
 	for (float d = 0.0; d < pi; d += pi / directions)
 	{
 		for (float i = 1.0 / quality; i <= 1.0; i += 1.0 / quality)
 		{
-			color += texture(tex, uv + vec2(cos(d), sin(d)) * radius * i);
+			color += texture(tex, uv + vec2(cos(d), sin(d)) * step_radius * i);
+			count += 1.0;
 		}
 	}
 
-	color /= quality * directions - directions + 1.0;
+	color /= count;
 
-	return color.xyz;
+	return mix(origin.xyz, color.xyz, strength);
 })";
 
 namespace Echo
