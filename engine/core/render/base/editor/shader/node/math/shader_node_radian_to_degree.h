327332145bbc71888afb21e2e701d8ad4720b838
Captain <qq79402005@gmail.com>
2021-01-26 23:42:53 +0800

    shader editor implement radians To Degrees node #938

---
+++ engine/core/render/base/editor/shader/node/math/shader_node_radian_to_degree.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "engine/core/render/base/editor/shader/node/shader_node.h"
+
+#ifdef ECHO_EDITOR_MODE
+
+namespace Echo
+{
+    class ShaderNodeRadianToDegree : public ShaderNode
+    {
+        ECHO_CLASS(ShaderNodeRadianToDegree, ShaderNode)
+
+    public:
+        ShaderNodeRadianToDegree();
+        virtual ~ShaderNodeRadianToDegree() {}
+
+		// name
+		virtual QString name() const override { return QStringLiteral("RadianToDegree"); }
+
+        // caption
+        virtual QString caption() const override { return QStringLiteral("Radian To Degree"); }
+
+        // is caption visible
+        virtual bool captionVisible() const override { return true; }
+
+		// category
+		virtual QString category() const override { return "Math"; }
+
+		// when input changed
+		virtual void setInData(std::shared_ptr<NodeData> nodeData, QtNodes::PortIndex port) override;
+
+		// generate code
+		virtual bool generateCode(ShaderCompiler& compiler) override;
+    };
+}
+
+#endif
