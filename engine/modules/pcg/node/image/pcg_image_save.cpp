6368668bc3e22483eec0bea904bb97535a3740d9
Captain <qq79402005@gmail.com>
2021-03-28 22:58:33 +0800

    pcg image save node #973

--- engine/modules/pcg/node/image/pcg_image_save.cpp
+++ engine/modules/pcg/node/image/pcg_image_save.cpp
@@ -1,5 +1,11 @@
 #include "pcg_image_save.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
+
 namespace Echo
 {
 	PCGImageSave::PCGImageSave()
@@ -31,12 +37,29 @@ namespace Echo
 
 	void PCGImageSave::run()
 	{
-
+		PCGDataPtr inputData = m_inputs[0]->getData();
+		if (inputData)
+		{
+			PCGImagePtr imageData = dynamic_cast<PCGImage*>(inputData.ptr());
+			save(imageData);
+		}
 	}
 
-	void PCGImageSave::save(PCGImagePtr Image)
+	// http://chanhaeng.blogspot.com/2018/12/how-to-use-stbimagewrite.html
+	void PCGImageSave::save(PCGImagePtr image)
 	{
+		if (image)
+		{
+			i32 idx = 0;
+			vector<ui8>::type pixels(image->getWidth()*image->getHeight());
 
+			for (const Color& color : image->getColors())
+			{
+				pixels[idx++] = color.r * 255.99f;
+			}
+
+			stbi_write_png("D:/test.png", image->getWidth(), image->getHeight(), 1, pixels.data(), image->getWidth() * 1);
+		}
 	}
 }
 
