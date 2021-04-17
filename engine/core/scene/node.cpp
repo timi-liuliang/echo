f92f0b684b630dc68caf70ee6e5e5040d5c785a0
Captain <qq79402005@gmail.com>
2021-04-18 02:15:02 +0800

    render window add rotation operation #1000

--- engine/core/scene/node.cpp
+++ engine/core/scene/node.cpp
@@ -453,6 +453,7 @@ namespace Echo
 		CLASS_BIND_METHOD(Node, setLocalPosition,		DEF_METHOD("setLocalPosition"));
 		CLASS_BIND_METHOD(Node, setLocalYawPitchRoll,	DEF_METHOD("setLocalYawPitchRoll"));
 		CLASS_BIND_METHOD(Node, setLocalOrientation,	DEF_METHOD("setLocalOrientation"));
+		CLASS_BIND_METHOD(Node, getWorldOrientation,	DEF_METHOD("getWorldOrientation"));
 		CLASS_BIND_METHOD(Node, setWorldOrientation,	DEF_METHOD("setWorldOrientation"));
 		CLASS_BIND_METHOD(Node, setEnable,				DEF_METHOD("setEnable"));
 		CLASS_BIND_METHOD(Node, isEnable,				DEF_METHOD("isEnable"));
