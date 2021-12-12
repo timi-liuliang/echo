a7060479575a36a921058c0a5249c2a023f6d477
Captain <qq79402005@gmail.com>
2021-12-12 20:45:39 +0800

    use tensorflow lite try pix2pix #524

--- engine/modules/tflite/tflite_model.cpp
+++ engine/modules/tflite/tflite_model.cpp
@@ -1,3 +1,4 @@
+#include "thirdparty/google/tensorflow/lite/c/c_api_internal.h"
 #include "tflite_model.h"
 #include "tflite_module.h"
 #include "engine/core/io/io.h"
@@ -17,9 +18,16 @@ namespace Echo
 
 	void TFLiteModel::bindMethods()
 	{
+		CLASS_BIND_METHOD(TFLiteModel, invoke);
 		CLASS_BIND_METHOD(TFLiteModel, getModelRes);
 		CLASS_BIND_METHOD(TFLiteModel, setModelRes);
+		CLASS_BIND_METHOD(TFLiteModel, getInputCount);
+		CLASS_BIND_METHOD(TFLiteModel, setInputCount);
+		CLASS_BIND_METHOD(TFLiteModel, getOutputCount);
+		CLASS_BIND_METHOD(TFLiteModel, setOutputCount);
 
+		CLASS_REGISTER_PROPERTY(TFLiteModel, "InputCount", Variant::Type::Int, getInputCount, setInputCount);
+		CLASS_REGISTER_PROPERTY(TFLiteModel, "OutputCount", Variant::Type::Int, getOutputCount, setOutputCount);
 		CLASS_REGISTER_PROPERTY(TFLiteModel, "Model", Variant::Type::ResourcePath, getModelRes, setModelRes);
 	}
 
@@ -33,12 +41,35 @@ namespace Echo
 				m_model = TfLiteModelCreate(memReader.getData<const char*>(), memReader.getSize());
 				if (m_model)
 				{
+					TfLiteInterpreterOptions* options = TfLiteInterpreterOptionsCreate();
+					TfLiteInterpreterOptionsSetNumThreads(options, 1);
 
+					m_interpreter = TfLiteInterpreterCreate(m_model, options);
+					if (m_interpreter)
+					{
+						m_inputCount = TfLiteInterpreterGetInputTensorCount(m_interpreter);
+						m_outputCount = TfLiteInterpreterGetOutputTensorCount(m_interpreter);
+					}
+					else
+					{
+						EchoLogError("TFLiteModel Failed to create interpreter");
+					}
 				}
 				else
 				{
-					EchoLogError("Failed to load tflite model [%s]", path.getPath().c_str());
+					EchoLogError("TFLiteModel Failed to load tflite model [%s]", path.getPath().c_str());
 				}
+			}
+		}
+	}
+
+	void TFLiteModel::invoke()
+	{
+		if (m_interpreter)
+		{
+			if (kTfLiteOk != TfLiteInterpreterInvoke(m_interpreter))
+			{
+
 			}
 		}
 	}
--- engine/modules/tflite/tflite_model.h
+++ engine/modules/tflite/tflite_model.h
@@ -13,14 +13,27 @@ namespace Echo
 		TFLiteModel();
 		virtual ~TFLiteModel();
 
-		// model file
+		// Model file
 		void setModelRes(const ResourcePath& path);
 		const ResourcePath& getModelRes() { return m_modelRes; }
+
+		// Input count
+		i32 getInputCount() const { return m_inputCount; }
+		void setInputCount() {}
+
+		// Output count
+		i32 getOutputCount() const { return m_outputCount; }
+		void setOutputCount() {}
+
+		// Invoke
+		void invoke();
 
 	public:
 		ResourcePath			m_modelRes = ResourcePath("", ".tflite");
 		TfLiteModel*			m_model = nullptr;
 		TfLiteInterpreter*		m_interpreter = nullptr;
+		i32						m_inputCount = 0;
+		i32						m_outputCount = 0;
 		vector<TfLiteTensor*>	m_inputs;
 		vector<TfLiteTensor*>	m_outputs;
 	};
