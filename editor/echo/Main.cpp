ea9f8334fd60a44fca55d0eb60b6a24134a6994a
Captain <qq79402005@gmail.com>
2023-01-06 02:18:56 +0800

    Mouse right button down menu - Generate Visual Studio project files #1232

--- editor/echo/Main.cpp
+++ editor/echo/Main.cpp
@@ -14,13 +14,19 @@
 
 int main( int argc, char* argv[])
 {
-	// default icon
-	QSettings settings;
-
 	// set directory path
     Echo::String filePath = QFileInfo(argv[0]).path().toStdString().c_str();
 	QDir::setCurrent(filePath.c_str());
 	QApplication::setLibraryPaths(QStringList() << QApplication::libraryPaths() << QDir::currentPath().append("/plugins/Qt"));
+
+#ifdef ECHO_PLATFORM_WINDOWS
+	// Generate Visual Studio project files
+	QSettings regGv("HKEY_CLASSES_ROOT\\.echo\\shell\\Generate Visual Studio Files\\command", QSettings::NativeFormat);
+	regGv.setValue("Default", (Echo::String(argv[0]) + " gen_vs %1").c_str());
+
+	QSettings regIcon("HKEY_CLASSES_ROOT\\.echo\\shell\\Generate Visual Studio Files", QSettings::NativeFormat);
+	regIcon.setValue("Icon", Echo::StringUtil::Format("%s", argv[0]).c_str());
+#endif
 
 	// parse & run
 	Echo::CMDLine::Parser(argc, argv);
