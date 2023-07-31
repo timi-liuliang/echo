dfc44d3f573292d778ccfc96c50229348d38a6aa
Captain <qq79402005@gmail.com>
2023-07-31 22:18:29 +0800

    .echo link - Generate visual studio project files #1282
    .echo link - Generate visual studio project files #1282
    
    .echo link - Generate visual studio project files #1282

--- editor/echo/Editor/Logic/CMDLine/RegEditMode.h
+++ editor/echo/Editor/Logic/CMDLine/RegEditMode.h
@@ -12,5 +12,15 @@ namespace Echo
 	public:
 		// Exec command
 		bool exec(int argc, char* argv[]);
+
+	public:
+		// Set default value
+		static void setDefaultValue(const String& regPath, const String& value);
+
+		// Add right menu
+		static void addRightMenu(const String& shellPath, const String& name, const String& desc, const String& icon, const String& command);
+
+		// Is registered
+		static void check(const char* echoExe);
 	};
 }
